ef17cc5f4b0ab7bb0bd2caa72f0cedf001144345
julian curran <julian_curran@hotmail.com>
2017-03-02 18:35:33 -0500

    Finished error fixing

--- Final Project/Final Project/Enemy.cpp
+++ Final Project/Final Project/Enemy.cpp
@@ -2,11 +2,11 @@
 
 
 
-Enemy::Enemy() : Entity(400, 300, 20, 20) {
+Enemy::Enemy() : Actor(400, 300, 20, 20) {
 
 }
 
-Enemy::Enemy(sf::Vector2f startingPosition) : Entity(400, 300, 20, 20) {
+Enemy::Enemy(sf::Vector2f startingPosition) : Actor(400, 300, 20, 20) {
 	position = startingPosition;
 	homePosition = startingPosition;
 	velocity.x = 70;
@@ -95,102 +95,3 @@ void Enemy::hit(Entity* hitThis) {
 }
 
 //items = { new Item("name", HashMap<Integer, Integer>{2:1, 3:2}),
-=======
-#include "Enemy.h"
-
-
-
-Enemy::Enemy() : Entity(400, 300, 20, 20) {
-
-}
-
-Enemy::Enemy(sf::Vector2f startingPosition) : Entity(400, 300, 20, 20) {
-	position = startingPosition;
-	homePosition = startingPosition;
-	velocity.x = 70;
-	moving = true;
-	behaviorState = 1;
-
-}
-
-
-Enemy::~Enemy() { }
-
-
-
-void Enemy::update(float deltaTime) {
-
-
-
-	if (moving)
-		position.x += velocity.x * deltaTime;
-
-	position.y += velocity.y * deltaTime;
-
-	velocity.y += 50 * deltaTime;
-
-	if (velocity.y > 300)
-		velocity.y = 300;
-
-	hitBox.setPosition(position);
-}
-
-
-void Enemy::updateBehavior(float deltaTime, Entity* target, int type) {
-	
-	switch (behaviorState) {
-
-	case 1:
-
-		if ( sqrt(pow(homePosition.x - position.x, 2) + pow(homePosition.y - position.y, 2)) > 200) {
-			if (position.x > homePosition.x) {
-				velocity.x = -70;
-			}
-			else {
-				velocity.x = 70;
-			}
-		}
-
-		else if (sqrt(pow(target->position.x - position.x, 2) + pow(target->position.y - position.y, 2)) < 100.0) {
-			behaviorState = 2;
-		}
-		break;
-	case 2:
-
-		if (sqrt(pow(target->position.x - position.x, 2) + pow(target->position.y - position.y, 2)) < 32 && type == 1) {
-			//Attack();
-			moving = false;
-		}
-
-		else if (sqrt(pow(target->position.x - position.x, 2) + pow(target->position.y - position.y, 2)) > 400) {
-			behaviorState = 3;
-		}
-
-		break;
-	case 3:
-		if ( sqrt(pow(homePosition.x - position.x, 2) + pow(homePosition.y - position.y, 2)) < 64) {
-			behaviorState = 1;
-		}
-		break;
-	case 4:
-		if (attackTime < 1) {
-			moving = true;
-			behaviorState = 2;
-		}
-		else
-			attackTime -= deltaTime;
-		break;
-	}
-
-	update(deltaTime);
-
-}
-
-
-
-void Enemy::hit(Entity* hitThis) {
-
-}
-
-//items = { new Item("name", HashMap<Integer, Integer>{2:1, 3:2}),
-//		new Item("name2", HashMap<Integer, Integer>{2:1, 3 : 2}), }
--- Final Project/Final Project/Enemy.h
+++ Final Project/Final Project/Enemy.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "Entity.h"
-class Enemy : public Entity {
+#include "Actor.h"
+class Enemy : public Actor {
 public:
 	Enemy();
 	Enemy(sf::Vector2f);
@@ -18,50 +18,6 @@ public:
 
 	void hit(Entity*);
 
-	sf::Vector2f velocity;
-
-	//Keeps track of where the enemy was originally spawned
-	sf::Vector2f homePosition;
-
-	bool moving;
-
-	//Time spent attacking
-	float attackTime;
-
-	/*  An integer switch used to determine how the enemy AI should behave.
-		Current behavior settings are:
-		1: Enemy paces back and forth accross its starting point.
-		2: Enemy has been alterted by proximity to another object (ie sound or player)
-		3: Enemy is returning to its home position. For when the enemy loses sight of
-		   the player or doesn't find anything when investigating a sound.
-	*/
-	int behaviorState;
-
-	bool jump = false;
-};
-
-=======
-#pragma once
-#include "Entity.h"
-class Enemy : public Entity {
-public:
-	Enemy();
-	Enemy(sf::Vector2f);
-
-
-	~Enemy();
-
-	void update(float);
-
-	/*
-		Alternate update that takes an entity pointer to an object that
-		grabs the enemy's attention, and an int to indicate what the thing is.
-	*/
-	void Enemy::updateBehavior(float, Entity*, int);
-
-	void hit(Entity*);
-
-	sf::Vector2f velocity;
 
 	//Keeps track of where the enemy was originally spawned
 	sf::Vector2f homePosition;
